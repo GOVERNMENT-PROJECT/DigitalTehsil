a8912ff1ad0e276c8fed2bfae556b1578b88f7df
ashishprasad2310 <ashishprasadranjan@gmail.com>
2017-03-18 23:01:01 +0530

    commit 2

--- src/main/webapp/Birth Certificate.js
+++ src/main/webapp/Birth Certificate.js
@@ -3,29 +3,13 @@ var app = angular.module("BirthCertificate", []);
 app.controller("MainCtrl", ["$log","$http","$scope", 
    function($log,$http,$scope)
    {
+	
 	$log.log('This is log 1.');
-
-	var obj=
-	{
-		"Name" :$scope.Name,
-		"Date" :$scope.Date,
-		"Sex" :$scope.Sex,
-		"FathersName" : $scope.FathersName,
-		"FatherAadhaarNumber" : $scope.FatherAadhaarNumber,
-		"MothersName" : $scope.MothersName,
-		"Country" : $scope.Country,
-		"State" : $scope.State,
-		"District"  : $scope.District,
-		"Tehsil" : $scope.Tehsil,
-		"PlaceOfBirth" : $scope.PlaceOfBirth
-	};
 	
 	$scope.Submit = function()
-	  {		
+	  {	
 	    $log.log('This is log 2.');
-	    //$log.log(obj);
-	    $log.log($scope.name);
-	    $log.log('This is log 3.');
+	    
 	    var parameter = JSON.stringify({
 			"name" :$scope.name,
 			"date" :$scope.date,
@@ -39,43 +23,27 @@ app.controller("MainCtrl", ["$log","$http","$scope",
 			"tehsil" : $scope.tehsil,
 			"placeOfBirth" : $scope.placeOfBirth
 		});
+	    
 	    $log.log('This is log 4.');
 	    $log.log(parameter);
-	    
-	   /* $http({
-	        url: "http://localhost:8080/e-Tehsil/webapp/register/newborn",
-	        method: "POST",
-	        headers: { "Content-Type": "application/json" },
-	        obj
-	    });*/
-	    
-		$http.post("http://localhost:8080/e-Tehsil/webapp/register/newborn",parameter,{
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(function mySucces(response) 
-        	{
-        		$log.log("We are Successful");
-        		$log.log(response);
-        		$log.log(response.data);
-            	$scope.myWelcome = response.data;
-        	}, 
-        	function myError(response) 
-        	{
-        		$log.log("Ohh no!");
-        		$scope.myWelcome = response.statusText;
-        	});
-        
-        
-        /*success(
-			function(response)
-			{
-				  $log.log(response.data);
-				  $log.log(response);
-				  $log.log('This is log 5.');
-		      });
-		      */
-	  }
+	   
+		$http.post("http://localhost:8080/e-Tehsil/webapp/register/newborn", parameter,
+					{
+            			headers: { 'Content-Type': 'application/json' }
+					}).then(function mySucces(response) 
+							{
+								$log.log("We are Successful");
+								$log.log(response);
+								$log.log(response.data);
+								$scope.myWelcome = response.data;
+							}, 
+							
+							function myError(response) 
+							{
+								$log.log("Ohh no!");
+								$scope.myWelcome = response.statusText;
+							});
+	  	}
 	
    }]);
                             
