c0b750422a8ba05ddd6d81a2f47a2a4874817064
ashishprasad2310 <ashishprasadranjan@gmail.com>
2017-05-04 19:30:24 +0530

    load17

---
+++ src/main/webapp/controller/DomicileCertificate.js
@@ -0,0 +1,20 @@
+angular.module('HomeApp')
+	.controller('DomicileCertificate', function ($scope,$http,$log, $cookies) {
+		
+	  $scope.apply = function() {
+	    $http.get("http://localhost:8080/e-Tehsil/webapp/register/domicilecertificate/"+$cookies.get("LoginId"), 
+	      {	headers: { 'Content-Type': 'application/json' } }).
+	          then(function mySucces(response) {
+					$log.log("We are Successful");
+					$log.log(response);
+					$log.log(response.data);
+					$scope.myWelcome = response.data;
+					window.location="/e-Tehsil/HtmlFile/Success.html";
+				}, 
+				function myError(response) {
+					$log.log("Ohh no!");
+					$scope.myWelcome = response.statusText;
+				}); 
+	    };
+
+});
