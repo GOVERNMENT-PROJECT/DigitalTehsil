bbd6a1f43efbccaa256b7e70bf9c7b9c846bb211
ashishprasad2310 <ashishprasadranjan@gmail.com>
2017-05-05 23:24:27 +0530

    commit 21

---
+++ src/main/webapp/Admin/Controller/OldAgePension.js
@@ -0,0 +1,35 @@
+angular.module('AdminHomeApp')
+	.controller('OldAgePension', function ($scope,$http,$log, $cookies) {
+
+	   $http.get("http://localhost:8080/e-Tehsil/webapp/Admin/OldAgePensionCertificateRequest", 
+	      {	headers: { 'Content-Type': 'application/json' } }).
+	      		then(function mySucces(response) {
+	      			$log.log("We are Successful");
+					$log.log(response);
+					$log.log(response.data);
+					$scope.User = response.data;
+				}, 
+				function myError(response) {
+					$log.log("Ohh no!");
+				}); 
+	   
+	   $scope.Save=function(){
+		   	var obj = JSON.stringify($scope.User);
+	   		$http.post("http://localhost:8080/e-Tehsil/webapp/Admin/OldAgePensionCertificateApproval", obj,
+				{ headers: { 'Content-Type': 'application/json' } }).
+				   	then(function mySucces(response) {
+				      	$log.log("We are Successful");
+						$log.log(obj);
+						$log.log(response.data);
+						$scope.User = response.data;
+						window.alert("success reached!");
+						window.location="AdminHome.html#/Forms";
+					}, 
+					function myError(response) {
+						$log.log("Ohh no!");
+						window.alert("faliure occurs");
+						window.location="AdminHome.html#/OldAgePension";
+					}); 
+	   };
+				
+});
